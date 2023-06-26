13481c6f4f3676d167a789c73ad30ab556d41cb0
Aniket Chowdhury <8.aniket.chowdhury@gmail.com>
2023-06-27 05:29:49 +0530

    feat: uncommited files

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,11 +1,8 @@
-import { useAuth } from "@app/hooks/useAuth";
-import { Roles } from "@root/core";
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-  const { role } = useAuth();
 
-  return <>{(role === Roles.None || role === Roles.User) && <></>}</>;
+  return <></>;
 };
 
 export default Home;
